3b67040c6cee41e49f258a111a1cf7e0f51a3016
Losses <losses.don@gmail.com>
2021-03-20 12:40:40 +0800

    feat: Extract types chain to independent package

---
+++ packages/types-chain/src/index.ts
@@ -0,0 +1,10 @@
+// Copyright 2017-2020 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import Phala3 from './phala-poc3';
+
+// alphabetical
+export const typesChain = {
+  'Phala PoC-3': Phala3,
+  'Phala PoC-4': Phala3,
+};
