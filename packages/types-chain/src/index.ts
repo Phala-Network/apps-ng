ecb5d241ca1c287ba179b7deaad05067df4a6266
Losses <losses.don@gmail.com>
2021-03-29 19:32:59 +0800

    chore: Update types def

--- packages/types-chain/src/index.ts
+++ packages/types-chain/src/index.ts
@@ -2,10 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import Phala3 from './phala-poc3';
+import phalaTypeDef from './phala-typedef';
 
 // alphabetical
 export const typesChain = {
   'Phala PoC-3': Phala3,
   'Phala PoC-4': Phala3,
   'Phala Local Testnet': Phala3,
+  'Integration Test': phalaTypeDef,
+  'Staging Testnet': phalaTypeDef,
+  'Phala Development': phalaTypeDef,
+  'Local Testnet': phalaTypeDef,
 };
